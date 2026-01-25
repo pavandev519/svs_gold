9f72ce637b0602f19f324fe6496cc3ab39407da3
Pavan <you@example.com>
2026-01-25 21:13:31 +0530

    deploy

--- db.py
+++ db.py
@@ -5,6 +5,7 @@ import os
 DATABASE_URL = (
     "postgresql://postgres.nuoiyydeurtxqtlzwutq:CxmZJCnw4qvBsFQE@aws-1-ap-south-1.pooler.supabase.com:6543/postgres"
     )
+
 def get_connection():
     return psycopg2.connect(
         DATABASE_URL,
