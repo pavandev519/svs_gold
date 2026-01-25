d7d6486ee0884ebc3d13c94fda79694599525788
Pavan <you@example.com>
2026-01-25 21:55:26 +0530

    deploy

--- db.py
+++ db.py
@@ -2,14 +2,13 @@ import psycopg2
 import os
 
 
-DATABASE_URL = (
-    "postgresql://postgres.nuoiyydeurtxqtlzwutq:L3H3qy728jNlduwl@aws-1-ap-south-1.pooler.supabase.com:6543/postgres"
-    )
+# DATABASE_URL = (
+#     "postgresql://postgres.nuoiyydeurtxqtlzwutq:L3H3qy728jNlduwl@aws-1-ap-south-1.pooler.supabase.com:6543/postgres"
+#     )
 
-
+DATABASE_URL = "postgresql://postgres.nuoiyydeurtxqtlzwutq:L3H3qy728jNlduwl@aws-1-ap-south-1.pooler.supabase.com:5432/postgres"
 def get_connection():
     return psycopg2.connect(
         DATABASE_URL,
-        connect_timeout=5,
-        sslmode="require"
+        connect_timeout=5
     )
