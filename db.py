f3f012eb2222298ebe5a7bf1364c839dc05e7f09
Pavan <you@example.com>
2026-01-26 08:51:16 +0530

    deploy_new

--- db.py
+++ db.py
@@ -2,9 +2,11 @@ import psycopg2
 import os
 
 
-DATABASE_URL = (
-    "postgresql://postgres.nuoiyydeurtxqtlzwutq:L3H3qy728jNlduwl@aws-1-ap-south-1.pooler.supabase.com:6543/postgres"
-    )
+# DATABASE_URL = (
+#     "postgresql://postgres.nuoiyydeurtxqtlzwutq:L3H3qy728jNlduwl@aws-1-ap-south-1.pooler.supabase.com:6543/postgres"
+#     )
+
+DATABASE_URL = "postgresql://neondb_owner:npg_fZ34VQeJLgON@ep-lively-haze-ahmx92ro-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 
 # DATABASE_URL = "postgresql://postgres.nuoiyydeurtxqtlzwutq:L3H3qy728jNlduwl@aws-1-ap-south-1.pooler.supabase.com:5432/postgres"
 def get_connection():
